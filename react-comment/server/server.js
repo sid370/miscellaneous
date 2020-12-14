21c77942b2dffd747939ac4d0f10486034909b62
Siddhant Tiwary <siddhanttiwary30@gmail.com>
2020-12-14 19:08:27 +0530

    socket established

--- react-comment/server/server.js
+++ react-comment/server/server.js
@@ -1,16 +1,26 @@
 const express = require ("express")
 const app = express()
 const port = 3001||process.env.PORT
+var cors = require ("cors")
 
-const server = app.listen(port,()=>{
+app.use(cors())
+server = app.listen(port,()=>{
     console.log("server listening")
 })
 
-const io=require("socket.io-client")(server)
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message: "Server Working"
+    })
+})
+
+
+const io=require("socket.io")(server)
 
 io.on("connection",(socket)=>{
     console.log(socket.id)
+    
     socket.on("comment",(data)=>{
-        socket.broadcast.emit("comment",data)
+        socket.broadcast.emit("commentRecieved",data)
     })
 })
--- react-comment/src/App.js
+++ react-comment/src/App.js
@@ -1,11 +1,84 @@
 import "./App.css";
 import { TextareaAutosize, Button } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
-import io from 'socket.io-client'
+import io from "socket.io-client";
 
 function getUsername() {
   const name = prompt("Enter your name");
   return name;
+}
+
+let comments = [];
+
+export default function App() {
+  let text = "";
+  var socket = io("localhost:3001");
+  const [state, setState] = useState(false);
+  const [mainState,setMainState] = useState(false)
+
+  function broadcastComment(data) {
+    socket.emit("comment", data);
+  }
+
+  socket.on("commentRecieved", (data) => {
+    console.log(data);
+    comments.push(data);
+    setMainState(!mainState)
+    //setState(!state);
+    //createEl(data);
+  });
+  //socket.on("commentRecieved",()=>setState(!state))
+
+  async function createEl(data) {
+    let username = getUsername();
+    const append = {
+      username,
+      data,
+      time: "18:00 P.M.",
+    };
+    comments.push(append);
+    broadcastComment(append);
+    setMainState(!mainState)
+    //setState(!state);
+    //setComment((comment) => comment.concat(append));
+  }
+
+  return (
+    <div className="App">
+      <h1>Comment Box</h1>
+      <TextareaAutosize
+        aria-label="Write your comment here"
+        style={{ width: 600, padding: "2px" }}
+        rowsMin={3}
+        placeholder="Type"
+        onInput={(e) => (text = e.target.value)}
+      />
+      <br />
+      <Button
+        type="submit"
+        onClick={() => createEl(text)}
+        variant="contained"
+        color="secondary"
+      >
+        Post
+      </Button>
+      <h5 style={{ fontSize: 20 }}>Comments</h5>
+      <br />
+      {comments.map((data, i) => {
+        return (
+          <ul style={{ listStyleType: "none" }} key={i}>
+            <li style={{ color: "red" }}>{data.username}</li>
+            <br />
+            <li>{data.data}</li>
+            <br />
+            <li>{data.time}</li>
+            <hr style={{ width: "60%", height: "5px" }}></hr>
+          </ul>
+        );
+      })}
+      {}
+    </div>
+  );
 }
 
 // let username = "";
@@ -77,68 +150,3 @@ function getUsername() {
 // }
 
 // export default App;
-
-let comments = [];
-const socket = io();
-
-
-export default function App() {
-  let text = "";
-  const [comment, setComment] = useState([]);
-
-  async function createEl(data) {
-    let username = getUsername();
-    const append = {
-      username,
-      data,
-      time: "18:00 P.M.",
-    };
-    comments.push(append);
-    broadcastComment(append);
-    setComment((comment) => [...comment, append]);
-  }
-
-  function broadcastComment(data){
-    socket.emit("comment",data)
-  }
-
-  socket.on("comment",(data)=>{
-    createEl(data);
-  })
-
-  return (
-    <div className="App">
-      <h1>Comment Box</h1>
-      <TextareaAutosize
-        aria-label="Write your comment here"
-        style={{ width: 600, padding: "2px" }}
-        rowsMin={3}
-        placeholder="Type"
-        onInput={(e) => (text = e.target.value)}
-      />
-      <br />
-      <Button
-        type="submit"
-        onClick={() => createEl(text)}
-        variant="contained"
-        color="secondary"
-      >
-        Post
-      </Button>
-      <h5 style={{ fontSize: 20 }}>Comments</h5>
-      <br />
-      {comments.map((data, i) => {
-        return (
-          <ul style={{ listStyleType: "none" }} key={i}>
-            <li style={{ color: "red" }}>{data.username}</li>
-            <br />
-            <li>{data.data}</li>
-            <br />
-            <li>{data.time}</li>
-            <hr style={{ width: "60%", height: "5px" }}></hr>
-          </ul>
-        );
-      })}
-    </div>
-  );
-}
