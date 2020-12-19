6ed9b4fcd7e1b710118a45e7a529b9db53d7c367
Siddhant Tiwary <siddhanttiwary30@gmail.com>
2020-12-19 23:36:03 +0530

    socket-io react

--- react-comment/src/App.js
+++ react-comment/src/App.js
@@ -13,21 +13,19 @@ let comments = [];
 export default function App() {
   let text = "";
   var socket = io("localhost:3001");
-  const [state, setState] = useState(false);
   const [mainState,setMainState] = useState(false)
 
   function broadcastComment(data) {
     socket.emit("comment", data);
   }
 
-  socket.on("commentRecieved", (data) => {
+  useEffect(()=>{
+    socket.on("commentRecieved", (data) => {
     console.log(data);
     comments.push(data);
     setMainState(!mainState)
-    //setState(!state);
-    //createEl(data);
   });
-  //socket.on("commentRecieved",()=>setState(!state))
+  })
 
   async function createEl(data) {
     let username = getUsername();
@@ -39,8 +37,6 @@ export default function App() {
     comments.push(append);
     broadcastComment(append);
     setMainState(!mainState)
-    //setState(!state);
-    //setComment((comment) => comment.concat(append));
   }
 
   return (
@@ -80,73 +76,3 @@ export default function App() {
     </div>
   );
 }
-
-// let username = "";
-
-// let comments = [];
-// function App() {
-//   const [comment, setComment] = useState([]);
-//   let text = "";
-//   let oldcomment = "";
-
-//   function createEl() {
-//     oldcomment = comment;
-//     text=text.trim()
-//     let append = {
-//       username,
-//       text,
-//       time: "18:00",
-//     };
-//     if (text!==""){
-//     setComment(append);
-//     comments.push(append);
-//     }
-//   }
-
-//   function socketCom(data){
-//     oldcomment=comment
-//     let append={
-//       username,
-//       data,
-//       time: "18:00 P.M."
-//     };
-//     setComment(append)
-//     comments.push(append)
-//   }
-
-//   return (
-//     <div className="App">
-//       {username === "" ? (username = getUsername()) : null}
-//       <h1>Comment Box</h1>
-//       <TextareaAutosize
-//         aria-label="Write your comment here"
-//         style={{ width: 600, padding: "2px" }}
-//         rowsMin={3}
-//         placeholder="Type"
-//         onInput={(e) => (text = e.target.value)}
-//       />
-//       <br />
-//       <Button
-//         type="submit"
-//         onClick={() => createEl()}
-//         variant="contained"
-//         color="secondary"
-//       >
-//         Post
-//       </Button>
-//       <h5 style={{fontSize: 20}}>Comments</h5>
-//       <br />
-//       {oldcomment !== comment
-//         ? comments.map((data, index) =>
-//             <ul style={{ listStyleType: "none" }} key={index}>
-//                 <li>{data.username}</li><br/>
-//                 <li>{data.text}</li><br/>
-//                 <li>{data.time}</li>
-//             </ul>
-//           )
-//         : null}
-//     </div>
-//   );
-// }
-
-// export default App;
